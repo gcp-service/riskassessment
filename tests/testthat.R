6fafcb10204b4a0ff9a2a5941313085713f21929
LDSamson <l.d.samson@gmail.com>
2025-07-03 19:52:59 +0200

    Try fix test errors

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,15 +1,5 @@
 library(testthat)
-library(shiny)
 library(riskassessment)
-# Load application support files into testing environment
 library(shinytest2)
-options(shinytest2.load_timeout = 45*1000)
-options(shinytest2.timeout = 45*1000)
-
-tmpt <- Sys.time()
-while (!chromote::has_default_chromote_object() && Sys.time() - tmpt < 1) {
-  try(chromote::set_default_chromote_object(chromote::Chromote$new()), silent = TRUE)
-}
 
 test_check("riskassessment")
-# test_file("tests/testthat/test-downloadHandler.R")
