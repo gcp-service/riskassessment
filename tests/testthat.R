7f2dd4c3676c5f7220cfb04f909aa44688822018
LDSamson <l.d.samson@gmail.com>
2025-07-03 19:35:45 +0200

    Remove no-sandbox option

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,13 +6,6 @@ library(shinytest2)
 options(shinytest2.load_timeout = 45*1000)
 options(shinytest2.timeout = 45*1000)
 
-chromote::set_chrome_args(
-  c(
-    chromote::default_chrome_args(),
-    "--no-sandbox"
-  )
-)
-
 tmpt <- Sys.time()
 while (!chromote::has_default_chromote_object() && Sys.time() - tmpt < 1) {
   try(chromote::set_default_chromote_object(chromote::Chromote$new()), silent = TRUE)
